ca1fd714cd2a22cca25e1c5297f1cd6b3efb18db
hi_J <2463314384@qq.com>
2017-12-26 11:48:23 +0800

    靳宏伟

---
+++ post.js
@@ -0,0 +1,16 @@
+var http=require('http')
+
+
+http.createServer(function(req,res){
+
+	var str = '';
+	req.on('data',function(err,data){
+		str += data
+	});
+	req.on('end',function(err){
+		var json = querystring.parse(str)
+		console.log(json)
+	})
+
+
+}).listen(1111)
